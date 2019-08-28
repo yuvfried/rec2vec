ebcadd636a60b65c4a11a93d5d36875ee53ae575
arielmi85 <arielmi85@cs.huji.ac.il>
2019-08-28 12:24:45 +0300

    Cooking techniques stemming is operational

--- data_pre_processing.py
+++ data_pre_processing.py
@@ -1,16 +1,16 @@
 from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
+# from nltk.tokenize import sent_tokenize, word_tokenize
 import pandas as pd
 
-
-raw_techs = pd.read_csv("raw_techs.csv")
-# raw_techs = pd.DataFrame([["baking", "dry"],["frying", "wet"]])
-print(raw_techs.to_string())
-
 ps = PorterStemmer()
-raw_techs = raw_techs.apply(lambda x: ps.stem(x) if x)
-print(raw_techs)
-# print(raw_techs[["tech", "count"]])
+raw_techs = pd.read_csv("raw_techs.csv")
+rec_inst = pd.DataFrame([["baking", "dry"],["frying", "wet"]])
+
+# Create a new Series of STEMMED techniques
+raw_techs["stemmed"] = raw_techs["tech"].apply(lambda x: ps.stem(x))
+
+# STEM all the techniques in the recipe instructions DB
+
 
 # words = ["bak", "bake", "baking", "bakes"]
 # for word in words:
